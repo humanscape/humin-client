21a1de2a5ad66c5344177f388b95ed4b2cff098b
humanscape-june <june.333@humanscape.io>
2021-05-10 16:29:09 +0900

    fix: vscode에서 자동으로 추가된 코드 삭제

--- src/index.js
+++ src/index.js
@@ -2,23 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< Updated upstream
-import reportWebVitals from './reportWebVitals';
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
 import { createStore } from 'redux';
 import reducer from './store/modules';
 import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
-
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 ReactDOM.render(
   <Provider store={store}>
