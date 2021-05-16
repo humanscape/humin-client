d9eb44125d2edf1b83b65aa6e29ce5e65205dc91
humanscape-june <june.333@humanscape.io>
2021-05-16 21:12:32 +0900

    feat: 로그인 도메인에 따라 조직 분류

--- src/store/modules/Rooms.js
+++ src/store/modules/Rooms.js
@@ -1,6 +1,8 @@
+const DROP_ROOMS = "rooms-action/DROP_ROOMS";
 const SET_ROOMS = "rooms-action/SET_ROOMS";
 
 export const setRooms = rooms => ({type: SET_ROOMS, rooms});
+export const dropRooms = () => ({type: DROP_ROOMS});
 
 const initState = [];
 
@@ -8,6 +10,8 @@ export default function rooms(state = initState, action){
     switch(action.type){
         case SET_ROOMS:
             return action.rooms;
+        case DROP_ROOMS:
+            return [];
         default:
             return state;
     }
