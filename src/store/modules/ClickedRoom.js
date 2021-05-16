7bab9a5a72f8cb4a2c651ad692a1fd77a6a039d2
humanscape-june <june.333@humanscape.io>
2021-05-16 18:16:03 +0900

    style: roomId에서 roomName으로 변경

--- src/store/modules/ClickedRoom.js
+++ src/store/modules/ClickedRoom.js
@@ -1,7 +1,7 @@
 const SET_CLICKED_ROOM = "clicked-room/SET_ROOM";
 const DROP_CLICKED_ROOM = "clicked-room/DROP_ROOM";
 
-export const setClickedRoom = roomId => ({type: SET_CLICKED_ROOM, roomId});
+export const setClickedRoom = roomName => ({type: SET_CLICKED_ROOM, roomName});
 export const dropClickedRoom = () => ({type: DROP_CLICKED_ROOM});
 
 const initState = null;
@@ -9,7 +9,7 @@ const initState = null;
 export default function userProfile(state = initState, action){
     switch(action.type){
         case SET_CLICKED_ROOM:
-            return action.roomId;
+            return action.roomName;
         case DROP_CLICKED_ROOM:
             return null;
         default:
