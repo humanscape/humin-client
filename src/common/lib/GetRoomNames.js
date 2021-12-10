f6c4246b17dd875ebc584c0ff9f8e4208cdd21f0
humanscape-june <june.333@humanscape.io>
2021-12-10 23:41:44 +0900

    feat: 모든 방 이용 가능하도록 수정

--- src/common/lib/GetRoomNames.js
+++ src/common/lib/GetRoomNames.js
@@ -1,19 +1,10 @@
 const roomList = {
-    public: ["PR", "C1", "C2", "C3", "휴방", "주방"],
-    humanscape : ["H1", "H2", "H3", "안방", "골방"],
-    mommytalk: ["M1", "M2", "M3"]
+    public: ["PR", "C1", "C2", "C3", "휴방", "주방","H1", "H2", "H3", "M2", "M3"]
 }
 
 function getRoomNames(organization){
-    if (organization==="all"){
+    if (organization==="humanscape"){
         return roomList.public.concat(roomList.humanscape, roomList.mommytalk);
-    }
-    else if (organization==="humanscape"){
-        return roomList.public.concat(roomList.humanscape);
-        
-    }
-    else if (organization==="mommytalk"){
-        return roomList.public.concat(roomList.mommytalk);
     }
     else {
         return [];
